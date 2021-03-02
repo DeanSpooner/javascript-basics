3a136de4b564f7b2b7fa0928df05b22fe229d613
Dean Spooner <dean.spooner@me.com>
2021-03-02 22:32:02 +0000

    Accidentally only committed test files and not src files, committing and pushing these now.

--- src/numbers.js
+++ src/numbers.js
@@ -1,45 +1,45 @@
 const add = (a, b) => {
-  // your code here
+  return a + b;
 };
 
 const subtract = (a, b) => {
-  // your code here
+  return a - b;
 };
 
 const multiply = (a, b) => {
-  // your code here
+  return a * b;
 };
 
 const divide = (a, b) => {
-  // your code here
+  return a / b;
 };
 
 const power = (a, b) => {
-  // your code here
+  return a ** b;
 };
 
 const round = a => {
-  // your code here
+  return Math.round(a);
 };
 
 const roundUp = a => {
-  // your code here
+  return Math.ceil(a);
 };
 
 const roundDown = a => {
-  // your code here
+  return Math.floor(a);
 };
 
 const absolute = a => {
-  // your code here
+  return Math.abs(a); //Absolute is the positive version of an integer, whether it's + or -. ±3 > 3.
 };
 
 const quotient = (a, b) => {
-  // your code here
-};
+  return Math.trunc(a / b); //This is the part of a division answer that's not a remainder e.g. 20/3 > quotient of 6, remainder of 2.
+}; //Math.floor doesn't work here because the rounding would be wrong for a negative division! This truncates a float to a number, gets rid of the remainder.
 
 const remainder = (a, b) => {
-  // your code here
+  return a % b;
 };
 
 module.exports = {
